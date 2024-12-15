e4823c5115542399b98f080e03c93a30462ff95f
Fireog190 <firelight1og@gmail.com>
2024-12-15 18:39:47 +0100

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,44 +1,37 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
+import FormData from "form-data";
 
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-    if (!text) throw `*Enter a song name!*\n\n*Example:*\n${usedPrefix + command} behold by Hillsong`
-    try {
-        let kyuu = await fetch(`https://api.agatz.xyz/api/ytsearch?message=${text}`);
-        let tylor = await fetch(`https://exonity.tech/api/ytdlp2-faster?apikey=adminsepuh&url=${kyuu.data[0].url}`);${kyuu.data[0].url}`);
-
-        // Prepare message template
-        let doc = {
-            audio: {
-                url: tylor.result.audio
-            },
-            mimetype: 'audio/mpeg',
-            fileName: `${tylor.result.title}.mp3`,
-            contextInfo: {
-                externalAdReply: {
-                    showAdAttribution: true,
-                    mediaType: 2,
-                    mediaUrl: `${tylor.result.audio}`,
-                    title: `FIRE MD`,
-                    body: `${tylor.result.title}.mp3`,
-                    sourceUrl: `${tylor.result.audio}`,
-                    thumbnail: `https://i.imgur.com/tStJm2M.jpeg`,
-                }
-            }
-        }
-
-        // Send audio file with metadata
-        await conn.sendMessage(m.chat, doc, { quoted: m })
-
-
-    } catch (error) {
-        console.error('Error in music download:', error)
-        m.reply(`An error occurred: ${error.message}\nPlease try again later`)
-    }
-}
-
-handler.help = ['play'].map(v => v + ' <query>')
-handler.tags = ['downloader']
-handler.command = /^(play|mp3|ytmusic)$/i
-
+let handler = async (m, { conn, usedPrefix, command }) => {
+	switch (command) {
+		case "play":
+			{
+				if (!text) throw ("What song do you want to download ");
+				let kyuu = await fetch(`https://api.agatz.xyz/api/ytsearch?message=${text}`);
+				let tylor = await fetch(`https://api.agatz.xyz/api/ytmp3?url=${kyuu.data[0].url}`);
+				await conn.sendMessage(
+					 m.chat,
+					 {
+					   audio: { url: tylor.data[0].downloadUrl },
+					   fileName: `${tylor.data[0].title}.mp3`,
+					   mimetype: "audio/mpeg",
+					   contextInfo: {
+				  externalAdReply: {
+					title: `FIRE MD`,
+					body: `${tylor.data[0].title}.mp3`,
+					thumbnailUrl: `https://i.imgur.com/tStJm2M.jpeg`,
+					sourceUrl: `${tylor.data[0].downloadUrl}`,
+					mediaType: 2,
+					showAdAttribution: true,
+					}
+				}
+			}, { quoted: m });
+		}
+			break;
+	}
+};
+handler.help = ["play"];
+handler.tags = ["downloader"];
+handler.command = ["play"];
 export default handler
 
+            
